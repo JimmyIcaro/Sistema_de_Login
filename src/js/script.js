b060458357912ae9c540e5429c51d637b43bfa4d
Jimmy Ícaro <159497295+JimmyIcaro@users.noreply.github.com>
2024-04-23 08:21:40 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ const senia = document.getElementById('senha')
 const butaum = document.getElementById('dentro')
 
 butaum.addEventListener('click', () => {
-    if(imaiu.value == 'admin@admin.com' && senia.value == 1234){
+    if(imaiu.value == 'admin@admin.com' & senia.value == 1234){
         alert('login successful')
     } else {
         alert('login failed')
